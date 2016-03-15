634d61155cd6074c13dd1b00544966260a656507
marc.asenjo <markasenjo@hotmail.com>
2016-03-15 13:06:10 +0100

    merdamain

--- Java/src/edu/upc/Main.java
+++ Java/src/edu/upc/Main.java
@@ -1,8 +1,18 @@
 package edu.upc;
 
+import IA.DistFS.Requests;
+import IA.DistFS.Servers;
+
 public class Main {
+    public static void main(String[] args) throws Servers.WrongParametersException {
+        int users = 200;
+        int requests = 5;
+        int seed = 0;
+        int nserv = 50;
+        int nrep = 5;
 
-    public static void main(String[] args) {
-
+        Requests r =  new Requests(users, requests, seed);
+        Servers s = new Servers(nserv, nrep, seed);
+        FileDistribSystem fds = new FileDistribSystem(s,r,users);
     }
 }
