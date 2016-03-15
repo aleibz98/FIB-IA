7bf490e80630633d16aa992be2bbdf2d92df1544
jmigual <j.marce.igual@gmail.com>
2016-03-15 11:02:43 +0100

    Added State class and added more documentation

---
+++ Java/src/edu/upc/State.java
@@ -0,0 +1,16 @@
+package edu.upc;
+
+/**
+ * Created by Joan on 15/03/2016.
+ */
+public class State {
+
+    public static int[][][] state;
+
+    /**
+     * Creates the initial state
+     */
+    public State() {
+
+    }
+}
