f940060a05e5dbc5616f8b43b3f02a0f44c89938
jmigual <j.marce.igual@gmail.com>
2016-03-19 21:40:06 +0100

    Added heuristic to Hill Climbing
    Finds the server with the maximum time and returns this value

--- Java/src/edu/upc/FDSHeuristicFunction.java
+++ Java/src/edu/upc/FDSHeuristicFunction.java
@@ -6,7 +6,14 @@ import aima.search.framework.HeuristicFunction;
  * Created by marc.asenjo on 17/03/2016.
  */
 public class FDSHeuristicFunction implements HeuristicFunction {
+
     public double getHeuristicValue(Object state) {
-        return 1;
+        FDS fds = (FDS) state;
+        int[] serverTimes = fds.getServertimes();
+        int max = 0;
+        for (int time : serverTimes) {
+            if (time > max) max = time;
+        }
+        return max;
     }
 }
