1ec8ef862d519eea743cef19530cf54bca3fe453
ImInSpace <ignigomoreno@hotmail.com>
2016-03-31 19:06:34 +0200

    Removed repetitions on successorFunction2, still out of memory

--- Java/src/edu/upc/FDSSuccessorFunction2.java
+++ Java/src/edu/upc/FDSSuccessorFunction2.java
@@ -13,12 +13,24 @@ public class FDSSuccessorFunction2 implements SuccessorFunction {
     public List getSuccessors(Object aState) {
         ArrayList<Object> retVal = new ArrayList<>();
         FDS state = (FDS) aState;
+        FDSHeuristicFunction heuristic = new FDSHeuristicFunction();
 
-        FDSSuccessorFunction simple = new FDSSuccessorFunction();
-        List firststep= simple.getSuccessors(state);
-        for (Object o : firststep){
-            Successor s = (Successor) o;
-            retVal.addAll(simple.getSuccessors(((Successor) o).getState()));
+        for (int uid1 = 0; uid1 < state.getNUsers(); ++uid1) {
+            for (int rid1 = 0; rid1 < state.getNRequests(uid1); ++rid1) {
+                for (int sid1 : FDS.getServers().fileLocations(state.getFid(uid1, rid1))) {
+                    FDS newState1 = new FDS(state);
+                    newState1.swapServer(uid1, rid1, sid1);
+                    for (int uid2 = uid1; uid2 < state.getNUsers(); ++uid2) {
+                        for (int rid2 = rid1 + 1; rid2 < state.getNRequests(uid2); ++rid2) {
+                            for (int sid2 : FDS.getServers().fileLocations(state.getFid(uid2, rid2))) {
+                                FDS newState2 = new FDS(newState1);
+                                newState2.swapServer(uid2, rid2, sid2);
+                                retVal.add(new Successor("", newState2));
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         return retVal;
